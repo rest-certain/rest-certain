148dc5891e6d3ddeb0de29008f097e0986675fdb
Ben Ramsey <ben@ramsey.dev>
2024-12-26 15:31:46 +0000

    chore: initialize project using ramsey/php-library-starter-kit

---
+++ src/Example.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of rest-certain/rest-certain
+ *
+ * rest-certain/rest-certain is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either version
+ * 3 of the License, or (at your option) any later version.
+ *
+ * rest-certain/rest-certain is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with rest-certain/rest-certain.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) REST Certain Contributors <contributors@rest-certain.dev>
+ * @license https://opensource.org/license/lgpl-3-0/ GNU Lesser General Public License version 3 or later
+ */
+
+declare(strict_types=1);
+
+namespace RestCertain;
+
+/**
+ * An example class to act as a starting point for developing your library
+ */
+class Example
+{
+    /**
+     * Returns a greeting statement using the provided name
+     */
+    public function greet(string $name = 'World'): string
+    {
+        return "Hello, {$name}!";
+    }
+}
