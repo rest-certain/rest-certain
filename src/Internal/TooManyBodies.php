0eeb2e75a291811be30ed2a036b7dcb982f63e8c
Ben Ramsey <ben@ramsey.dev>
2025-03-29 04:01:29 +0000

    feat: create base exception and add TooManyBodies

---
+++ src/Internal/TooManyBodies.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * This file is part of REST Certain
+ *
+ * REST Certain is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * REST Certain is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with REST Certain. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) REST Certain Contributors <https://rest-certain.dev>
+ * @license https://opensource.org/license/lgpl-3-0/ GNU Lesser General Public License version 3 or later
+ */
+
+declare(strict_types=1);
+
+namespace RestCertain\Internal;
+
+use LogicException;
+use RestCertain\RestCertainException;
+
+/**
+ * The request has more than one body, which is too many.
+ */
+class TooManyBodies extends LogicException implements RestCertainException
+{
+}
